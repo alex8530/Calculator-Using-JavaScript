1504c0fb46ac23d282f19f544d1c0ead3a8ce10e
Alex <Aliabuharb8530@gmil.com>
2019-06-18 01:45:10 +0300

    styling code

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,8 @@
 //get all digit with .
 var keys  = document.querySelectorAll('.numbers .key');
 var operators = ['+', '-', '*', '/'];
-
+var inputTyping = document.querySelector('#input-typ') ; 
+var inputResult = document.querySelector('#input-result') ; 
 
 //add evnet listoner for all digit
 for (var i = 0; i < keys.length; i++) {
@@ -10,58 +11,49 @@ for (var i = 0; i < keys.length; i++) {
     });
   
 
-function getResult(valuePressedDigit){
+    function getResult(valuePressedDigit){
+        
+        //this valuePressedDigit will come when i press into the calculater , for example 0 ,1 ,2 ,+ ...
+        // If clear key is pressed , clear !
+        if(valuePressedDigit == 'C') {
+            inputTyping.innerHTML = ''; 
+            inputResult.innerHTML = ''; 
+        }else if(valuePressedDigit == '=') {
+            getResultWhenEqual();
+        }else if(operators.indexOf(valuePressedDigit) > -1) {
+            //check if key= operator
+        getResultWhenOperator(valuePressedDigit);
+        }else{
+            //any key number 
+            inputTyping.innerHTML +=  valuePressedDigit;
+        }
+    }
+}
+
+
+function getResultWhenEqual(){
+    var data = inputTyping.innerHTML;
+    var lastChar = data[data.length - 1];
+     
+    // If last character an operator or a decimal, remove it
+    if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+    data = data.replace(/.$/, '');
     
- 
-        //this valuePressedDigit will come when i press into the calculater , for example 0 ,1 ,2 ...
-        var inputTyping = document.querySelector('#input-typ') ; 
-        var inputResult = document.querySelector('#input-result') ; 
+    if(data) 
+         inputResult.innerHTML = eval(data);
+}
+  
+            
+function getResultWhenOperator(valuePressed ){
+    if( inputTyping.innerHTML == '' && valuePressed == '-')  
+    inputTyping.innerHTML += valuePressed;
+  var lastChar = inputTyping.innerHTML[inputTyping.innerHTML.length - 1];
+   //Add operator only if not enpty and last char not operator
+   if(inputTyping.innerHTML != '' && operators.indexOf(lastChar) == -1) 
+   inputTyping.innerHTML += valuePressed;
 
-
-     	// If clear key is pressed , clear !
-		if(valuePressedDigit == 'C') {
-			inputTyping.innerHTML = ''; 
-			inputResult.innerHTML = ''; 
-         }
-         
-         // If eval key is pressed, calculate and display the result
-         else if(valuePressedDigit == '=') {
-             var data = inputTyping.innerHTML;
-             var lastChar = data[data.length - 1];
-              
-             // Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
-             if(operators.indexOf(lastChar) > -1 || lastChar == '.')
-             data = data.replace(/.$/, '');
-             
-             if(data) 
-                  inputResult.innerHTML = eval(data);
-          }	
-          
-          else if(operators.indexOf(valuePressedDigit) > -1) {
-			// Operator is clicked
-			// Get the last character 
-			var lastChar = inputTyping.innerHTML[inputTyping.innerHTML.length - 1];
-			
-			 
-			 if( inputTyping.innerHTML == '' && valuePressedDigit == '-')  
-              inputTyping.innerHTML += valuePressedDigit;
- 
-			
-			 //Add operator only if not enpty and last char not operator
-             if(inputTyping.innerHTML != '' && operators.indexOf(lastChar) == -1) 
-             inputTyping.innerHTML += valuePressedDigit;
-
-			// Replace the last operator that i press
-			if(operators.indexOf(lastChar) > -1 &&  inputTyping.innerHTML.length > 1) {
- 				inputTyping.innerHTML = inputTyping.innerHTML.replace(/.$/, valuePressedDigit);
-			}
-			 
-		}
-          else{
-           inputTyping.innerHTML +=  valuePressedDigit;
-
-          }
-         
-
- }
+  // Replace the last operator that i press
+  if(operators.indexOf(lastChar) > -1 &&  inputTyping.innerHTML.length > 1) {
+       inputTyping.innerHTML = inputTyping.innerHTML.replace(/.$/, valuePressed);
+  }
 }
